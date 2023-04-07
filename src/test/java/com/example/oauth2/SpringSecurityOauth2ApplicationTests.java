4beca3ce81df9854fffd403747d76b73f3a3275b
ngan <nganknn3@gmail.com>
2023-04-07 16:25:46 +0700

    initial project

---
+++ src/test/java/com/example/oauth2/SpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
